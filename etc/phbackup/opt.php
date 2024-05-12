0255243a1aad5640f99139ba22cfda23ef35c6c5
jaredhared <jared@host4.biz>
2024-05-12 11:36:27 +0200

    Options update

--- etc/phbackup/opt.php
+++ etc/phbackup/opt.php
@@ -4,15 +4,18 @@
 $db_host = 'localhost';
 
 // Database name
-$db_name = 'phback';
+$db_name = 'phbackup';
 
 // Database username
-$db_user = 'phback_user';
+$db_user = 'phbackupuser';
 
 // Database password
-$db_pass = 'Cogb4FEmcKy4pnKP2jBOm';
+$db_pass = 'somepassword';
 
-// Backup path
+// Path to Rsync
+$cmd_rsync = '/usr/bin/rsync';
+
+// Backup path to store everything
 $backup_path = '/var/www/phbackup';
 
 // Paths to include by default
@@ -20,6 +23,7 @@ $default_include_paths="/etc
 /home
 /root
 /var/www
+/var/spool/cron
 /var/db-backup";
 
 // Paths to exclude by default
@@ -33,6 +37,8 @@ $default_pre_schedule="15 0 * * * root /opt/phbackup.sh > /var/log/phbackup_pre.
 $default_pre_script = '
 #!/bin/bash
 
+$mariabackup=`which mariabackup`
+
 MARIABACKUP=1
 MYSQLDUMP=0
 BACKUPPATH="/var/db-backup"
@@ -42,9 +48,9 @@ mkdir $BACKUPPATH
 
 if [ $MARIABACKUP -eq 1 ]; then
     echo -n "mariabackup starting..."
-    /usr/bin/mariabackup --backup --target-dir=$BACKUPPATH/mariabackup --user=root
+    $mariabackup --backup --target-dir=$BACKUPPATH/mariabackup --user=root
     echo -n "mariabackup prepare starting..."
-    /usr/bin/mariabackup --prepare --target-dir=$BACKUPPATH/mariabackup
+    $mariabackup --prepare --target-dir=$BACKUPPATH/mariabackup
     echo "mariabackup Done!"
 fi
 
